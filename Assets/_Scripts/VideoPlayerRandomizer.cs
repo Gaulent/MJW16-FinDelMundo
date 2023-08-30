4a4c7c387e5cd74ad5868fb0f9f6408ce1a422a4
Gaulent <jose.saucod@gmail.com>
2023-08-30 18:40:13 +0200

    Playing more random videos

--- Assets/_Scripts/VideoPlayerRandomizer.cs
+++ Assets/_Scripts/VideoPlayerRandomizer.cs
@@ -1,38 +1,54 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.Video;
+using Random = UnityEngine.Random;
 
 public class VideoPlayerRandomizer : MonoBehaviour
 {
     [SerializeField] private VideoClip[] myVideos;
     private VideoPlayer myPlayer;
+    private List<VideoClip> videoQueue;
 
     
     // Start is called before the first frame update
     void Start()
     {
+        if (myVideos.Length == 0)
+            Debug.LogError("VideoPlayer don't have any input video");
         myPlayer = GetComponent<VideoPlayer>();
         myPlayer.loopPointReached += EndReached;
         PlayRandomVideo(myPlayer);
     }   
 
+    VideoClip GetRandomVideo()
+    {
+        VideoClip vc;
+
+        if (videoQueue == null || videoQueue.Count == 0)
+            videoQueue = myVideos.OrderBy(a => Random.value).ToList();
+
+        vc = videoQueue[0];
+        videoQueue.RemoveAt(0);
+        return vc;
+    }
+
     void PlayRandomVideo(VideoPlayer vp)
     {
-        int videoIndex;
+        VideoClip vc = GetRandomVideo();
 
-        do // TODO: Peligroso
-        {
-            videoIndex = Random.Range(0, myVideos.Length - 1);
-        } while (vp.clip.name == myVideos[videoIndex].name);
+        // Discard current video if is actually the same
+        if (vp.clip.name == vc.name)
+            vc = GetRandomVideo();
 
-        vp.clip = myVideos[videoIndex];
-
+        vp.clip = vc;
         vp.Play();
     }
-    
 
-    void EndReached(UnityEngine.Video.VideoPlayer vp)
+
+    void EndReached(VideoPlayer vp)
     {
         PlayRandomVideo(vp);
     }
